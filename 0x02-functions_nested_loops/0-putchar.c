e04d6884a62e1fffbd8a502bb9cab054b137d939
MoyoSonola <moscodelectrical@gmail.com>
2022-10-19 21:01:23 -0700

    alx Tasks: 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Return: Always 0
+*/
+int main(void)
+{
+	int i;
+	char h[8] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(h[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
